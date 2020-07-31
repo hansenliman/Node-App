0963b5a36cf22f54ae054c418432b4fa5f31d8bf
Hansen <limanh@seattleu.edu>
2020-07-30 19:40:25 -0700

    Lots of changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes')
+const notes = require('./notes')
 const { demandOption } = require('yargs')
 
 // Customize yargs version
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -32,8 +31,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing a note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
 
--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,21 @@
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan holidy'
-}
+const fs = require('fs')
 
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON)
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSONString = dataBuffer.toString()
+const user = JSON.parse(dataJSONString)
+
+user.name = 'Liman'
+user.age = 24
+
+console.log(user)
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
